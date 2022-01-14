a3899db656ad10591a2a0463441d7b316fc51324
MaltsevNikita <mn121288@gmail.com>
2022-01-14 10:52:27 +0300

    Наёл порядок в табах, Написал ещё один таб (tab02)

--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,7 @@ const fonts = gulp.series(otfToTtf, ttfToWoff, fontStyle)
 // Основные задачи
 const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images))
 // Построение сценариев выполнения задач
-const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server))
+const dev = gulp.series(reset, mainTasks, gulp.parallel(server,watcher))
 const build = gulp.series(reset, mainTasks)
 const deployZIP = gulp.series(reset, mainTasks, zip)
 const deployFTP = gulp.series(reset, mainTasks, ftp)
