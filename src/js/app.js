f51e9fc616e2e19f1ba456168e13749551423343
MaltsevNikita <mn121288@gmail.com>
2022-04-14 15:09:15 +0300

    Промежуточный комит. Надо разобраться с изменение классов

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,23 @@ flsFunctions.isWebp()
 import Swiper, { Navigation, Pagination } from 'swiper'
 
 const swiper = new Swiper()
+
+// =========================================================
+const partnerTabs = document.getElementById('partnerTabs');
+const partnerTabsContent = document.querySelectorAll('.partner-tabs__body');
+// console.log(partnerTabs);
+// console.log(partnerTabsContent);
+
+const changeClass = el => {
+  console.log(el);
+  for(let i = 0; i < partnerTabs.children.length; i ++ ){
+    partnerTabs.children[i].classList.remove('--active');
+  }
+  
+  el.classList.add('--active');
+}
+
+partnerTabs.addEventListener('click', e => {
+  const currTab = e.target.dataset.btn;
+  changeClass(e.target);
+})
