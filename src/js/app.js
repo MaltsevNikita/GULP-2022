39ef17d8eb24cf6a57048c73f35f172d5bd14223
MaltsevNikita <mn121288@gmail.com>
2022-04-14 17:53:55 +0300

    Доделал табы. Теперь всё готово. Надо бы запомнить, что когда добавляю класс active, то в css надо через точку указывать доплнительный класс

--- src/js/app.js
+++ src/js/app.js
@@ -9,19 +9,26 @@ const swiper = new Swiper()
 // =========================================================
 const partnerTabs = document.getElementById('partnerTabs');
 const partnerTabsContent = document.querySelectorAll('.partner-tabs__body');
-// console.log(partnerTabs);
-// console.log(partnerTabsContent);
+
 
 const changeClass = el => {
   console.log(el);
-  for(let i = 0; i < partnerTabs.children.length; i ++ ){
+  for(let i = 0; i < partnerTabs.children.length; i++ ){
     partnerTabs.children[i].classList.remove('--active');
   }
-  
   el.classList.add('--active');
 }
+// console.log(partnerTabsContent);
 
 partnerTabs.addEventListener('click', e => {
   const currTab = e.target.dataset.btn;
   changeClass(e.target);
+  for(let i = 0; i < partnerTabsContent.length; i++){
+    partnerTabsContent[i].classList.remove('--active');
+    console.log(partnerTabsContent[i].dataset.content);
+    if(partnerTabsContent[i].dataset.content == currTab){
+      partnerTabsContent[i].classList.add('--active')
+    }
+  }
+  
 })
